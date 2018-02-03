21026a7f0a36dcf65079b10754a865ab6def6985
aishu <aishvenugopalan.gmail.com>
2018-02-02 19:44:02 -0800

    initial commit

---
+++ apps/ninjas/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+	name = models.CharField(max_length = 255)
+	city = models.CharField(max_length=255)
+	state = models.CharField(max_length=2)
+	desc = models.TextField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	def __unicode__(self):
+		return "id:" + str(self.id) + ",name:" + self.name + ",city:" + self.city + ",state:" + self.state + ",desc:" + str(self.desc)
+		
+
+class Ninja(models.Model):
+	first_name = models.CharField(max_length=255)
+	last_name = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	dojo = models.ForeignKey(Dojo, related_name="ninjas")
+	def __unicode__(self):
+		return "id:" + str(self.id) + ",first_name:" + self.first_name + ",last_name:" + self.last_name
+		
